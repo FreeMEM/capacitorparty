b5bcdefa17ef53b3c537442a11e6c6fc971900d7
Francisco Tapias (FreeMEM) <francisco.antonio@tapiasbravo.com>
2021-01-27 11:55:48 +0100

    removed debug prints

--- productions/views.py
+++ productions/views.py
@@ -17,9 +17,6 @@ def production(request, production_id):
     menu = Menus()
     production = get_object_or_404(Production, pk=production_id)
     inf = inflect.engine()
-    print("UEEEEEEEE1")
-    print(inf.ordinal(production.classification))
-    print("UEEEEEEEE2")
     return render(request, "productions/production.html", dict(main_menu=menu.main_menu, production=production, clasificacion=inf.ordinal(production.classification)))
 
 def upload(request):
