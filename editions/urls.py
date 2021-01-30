900900951ffd04a815b693c68460b86a6b69277d
Francisco Tapias (FreeMEM) <francisco.antonio@tapiasbravo.com>
2021-01-30 12:06:29 +0100

    cleaning urls

--- editions/urls.py
+++ editions/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('editions', views.editions, name="editions"),
-    # path('', views.myapp_index, name='myapp_index'),
-    # path('publications/', views.publications, name='publications'),
+
 ]
