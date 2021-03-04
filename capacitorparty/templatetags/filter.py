8f66678ae3377790c580b18ea3890a922806f79e
Francisco Tapias (FreeMEM) <francisco.antonio@tapiasbravo.com>
2021-03-04 13:43:09 +0100

    fixed problem lighting the correct menu option

--- capacitorparty/templatetags/filter.py
+++ capacitorparty/templatetags/filter.py
@@ -26,15 +26,12 @@ register.filter('last_element', last_element)
 
 @register.simple_tag(takes_context=True)
 def active(context, pattern_or_urlname):
-
     try:
-        pattern = '^' + reverse(pattern_or_urlname)
+        pattern = reverse(pattern_or_urlname)
     except NoReverseMatch:
         pattern = pattern_or_urlname
     path = context['request'].path
-    # pattern = pattern_or_urlname
-    # path = context['request'].path
-    print("pattern: %s path %s - %s" % (pattern, path, re.search(pattern, path)))
-    if re.search(pattern, path):
+
+    if pattern.split("/")[-1] == path.split("/")[-1]:
         return 'active'
     return ''
