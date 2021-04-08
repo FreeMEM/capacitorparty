16ffd54853411ce35b0d1f37738a2e7e39de4509
freemem <francisco@tapiasbravo.com>
2021-04-08 19:26:06 +0200

    link to edition done

--- capacitorparty/urls.py
+++ capacitorparty/urls.py
@@ -19,11 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
-    path('admin/', admin.site.urls),
-    path('productions/', include('productions.urls')),
-    path('', include('editions.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                path('admin/', admin.site.urls),
+                path('productions/', include('productions.urls')),
+                path('', include('editions.urls'))
+            ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
